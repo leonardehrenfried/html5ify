0d60f6d1dab30b79bf78b244486f65ba3e6beee0
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2011-10-04 23:13:50 +0200

    Initial commit

---
+++ amazon.js
@@ -0,0 +1,50 @@
+﻿/* 
+ * HTML5ify for Amazon Music
+ * Makes Amazon Music preview play in a <audio> tag instead of Flash
+ * Author: Leonard Ehrenfried <leonard.ehrenfried@web.de>
+ */
+
+var Amazon = {
+   
+  /*
+   * Fetches all links that are play buttons
+   */
+  getAnchors : function(){
+    var anchors = document.querySelectorAll(".trackList td.playcol>a");
+    return anchors;
+  },
+  
+  /**
+   * Attach events to the anchors so that the mu3 file is downloaded
+   */
+  attachEvents : function(anchors){
+    var that = this;
+
+    for (var i = 0; i < anchors.length; ++i) {
+      var anchor = anchors[i];
+      anchor.addEventListener('click', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        that.handleClick(e.target);
+      }, true);
+    }
+
+  },
+
+  handleClick : function(anchor){
+    //could be the image that the user clicked on
+    if(anchor.tagName.toLower !== 'a'){
+      anchor = anchor.parentNode;
+    }
+    console.log(anchor.href); 
+  },
+
+  run : function(){
+    var anchors = this.getAnchors();
+    this.attachEvents(anchors);
+  }
+
+};
+
+Amazon.run();
+
