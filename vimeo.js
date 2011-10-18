f0c38052a9e5f95254e5cb9083084d1099d1f46a
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2011-10-19 00:12:22 +0200

    Implement vimeo script

--- vimeo.js
+++ vimeo.js
@@ -1,9 +1,17 @@
-function toggle_html5_player(obj, on) {
-    if (on) {
-        setCookie('html_player', 1, 365);
-    } 
-    else {
-        setCookie('html_player', 0, 365);
+sites.push({
+  shouldRun : function(){
+    return window.location.href.indexOf("vimeo.com") > -1;
+  },
+
+  isHtml5 : function(){
+    return !!document.cookie.match("html_player=1");
+  },  
+
+  run : function(){
+    if(!this.isHtml5()){
+      document.cookie = "html_player=1; expires=Monday, 04-Apr-2021 05:00:00 GMT";
+      window.location.reload(true);
     }
-    reload_page();
+  }
 }
+);
